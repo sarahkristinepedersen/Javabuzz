c5f956e8ffea5d2d15288a1e36486b1780e2dc52
Sarah Kristine Pedersen <mail@sarahkp.dk>
2015-09-29 11:56:37 +0100

    All divisible functions completed and tests passing

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,100 +1,14 @@
-javaBuzz(function() {
+var Javabuzz = function() {};
 
+Javabuzz.prototype.isDivisibleByThree = function(number) {
+  return (number % 3 === 0);
+};
 
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+  return (number % 5 === 0);
+};
 
 
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var Javabuzz = function(){};
-
-// // Javabuzz.prototype.isDivisibleByThree = function(number) {
-// //   return true;
-// // };
-
-// Javabuzz.prototype.isDivisibleByThree = function(number) {
-//   return (number % 3 === 0);
-// };
-
-// Javabuzz.prototype.isDivisibleByFifteen = function(number) {
-//   return this._isDivisibleBy(number, 15);
-// };
-
-
-
-// Javabuzz.prototype.says = function(number) {
-//   if (this.isDivisibleByFifteen(number)) {
-//     return "Javabuzz";
-//   }
-//   if (this.isDivisibleByThree(number)) {
-//     return "Java";
-//   }
-//   if (this.isDivisibleByFive(number)) {
-//      return "Buzz";
-//   }
-//   return number;
-// };
+Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+  return (number % 15 === 0);
+};
